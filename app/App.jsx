854540386b8ddfb901bb651dd217cdc56d029550
Morten Friesgaard Christensen <friesgaard@gmail.com>
2016-02-29 14:01:08 +0100

    added files for running development server

---
+++ app/App.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+
+import JsonEditor from '../src/component/JsonEditor';
+import { parse } from '../src/tools/Helpers';
+
+class App extends Component{
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      json: undefined,
+      text: undefined,
+      errorText: undefined
+    }
+  }
+
+  onTextAreaChange = (event) => {
+    this.setState({text: event.target.value})
+  };
+
+  loadJson = (event) => {
+    this.setState(parse(this.state.text))
+  };
+
+  render () {
+    return (
+      <div>
+        <div>
+          <textarea type="text"
+                    placeholder="Paste json here"
+                    onChange={this.onTextAreaChange}/>
+          <button onClick={this.loadJson}>Load typed json</button>
+          {this.state.errorText && <div>{this.state.errorText}</div>}
+        </div>
+        <JsonEditor json={this.state.json} propagateChanges={this.update}/>
+      </div>
+    );
+  }
+}
+
+export default App;
